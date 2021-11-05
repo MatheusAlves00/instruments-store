cd84508bffb9ee71d38cbed1fb0c3f70cc078914
MatheusAlves00 <m_alves11@yahoo.com.br>
2021-11-04 23:10:26 -0300

    Creating action show, new, edit, create, update and destroy

--- test/controllers/instruments_controller_test.rb
+++ test/controllers/instruments_controller_test.rb
@@ -6,4 +6,34 @@ class InstrumentsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get show" do
+    get instruments_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get instruments_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get instruments_edit_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get instruments_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get instruments_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get instruments_destroy_url
+    assert_response :success
+  end
+
 end
