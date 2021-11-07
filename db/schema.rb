a5341d2416970ed6998acfd66816d7d1f0639c24
MatheusAlves00 <m_alves11@yahoo.com.br>
2021-11-06 21:48:36 -0300

    Create model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_004148) do
+ActiveRecord::Schema.define(version: 2021_11_07_004356) do
 
   create_table "instruments", force: :cascade do |t|
     t.string "name"
     t.string "model"
     t.string "brand"
     t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "birth_date"
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
