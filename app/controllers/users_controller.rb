8f6ea2fdd07f21c1fbd3896b57c2e26aee3c0f7b
MatheusAlves00 <m_alves11@yahoo.com.br>
2021-11-21 17:39:53 -0300

    Creating create action and correcting view new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,11 +15,24 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birth_date, :email, :password)
   end
 end
