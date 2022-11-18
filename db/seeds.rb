60ab505a224979e2ae0e23610f93a9e4f60b27d5
matheusAlves00 <m_alves11@yahoo.com.br>
2022-11-18 18:34:32 -0300

    Create data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Instrument.create!(
+  name: 'Violão Acústico 3/4',
+  model: 'FA-15',
+  brand: 'Fender',
+  price: 1234.00
+)
+
+Instrument.create!(
+  name: 'ContraBaixo Elétrico',
+  model: 'Classic Vibe 60s Precision Bass',
+  brand: 'Squier',
+  price: 4975.00
+)
+
+Instrument.create!(
+  name: 'Guitarra',
+  model: 'Pacifica 112 VII',
+  brand: 'Yamaha',
+  price: 835.00
+)
