87c8409a4bcb5c8e42d674a24a6b65ff3a1e2408
matheusalves00 <m_alves11@yahoo.com.br>
2024-01-30 15:32:09 -0300

    add login form with devise

--- db/migrate/20211107004356_create_users.rb
+++ db/migrate/20211107004356_create_users.rb
@@ -3,9 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.date :birth_date
-      t.string :email
-      t.string :password
+      t.date   :birth_date
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_204002) do
+ActiveRecord::Schema.define(version: 2024_01_17_211346) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,10 +47,15 @@ ActiveRecord::Schema.define(version: 2022_11_30_204002) do
     t.string "first_name"
     t.string "last_name"
     t.date "birth_date"
-    t.string "email"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
