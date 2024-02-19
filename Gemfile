fac13a7d84d3ccb1cabef16b84dab8f79eef32a9
matheusalves00 <m_alves11@yahoo.com.br>
2024-02-19 19:24:08 -0300

    update bootstrap and node modules

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'bootstrap', '~> 4.4.1'
+gem 'jquery-rails'
+
 gem 'webpacker'
 
 gem 'image_processing'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require bootstrap/dist/js/bootstrap
-//= require popper.js/dist/popper
+//= require jquery3
+//= require popper
+//= require bootstrap
