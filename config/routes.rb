755a9df67761dbecf4656427529755af220402da
matheusalves00 <m_alves11@yahoo.com.br>
2024-04-02 19:39:29 -0300

    Add route and new schema in db

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :instruments, :except => [:index, :show]
   resources :users, :except => [:new]
   resources :shop, controller: 'instruments', :only => [:index, :show]
+  resources :type_products
 
   get 'contacts', to: 'contacts#index'
 
