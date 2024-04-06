c2934135e2fb32ab5abe2fdbda3c0edb7a824c40
matheusalves00 <m_alves11@yahoo.com.br>
2024-04-06 10:12:58 -0300

    Create connection between instruments and product_type

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,6 +1,7 @@
 class InstrumentsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :admin?, only: [:new, :edit, :destroy]
+  before_action :set_product_type_options, only: [:new, :create, :edit, :update]
 
   def index
     @instruments = Instrument.all
@@ -23,7 +24,7 @@ class InstrumentsController < ApplicationController
     @instrument = Instrument.new(instrument_params)
 
     if @instrument.save
-      redirect_to @instrument
+      redirect_to shop_index_path 
     else
       render :new
     end
@@ -33,7 +34,7 @@ class InstrumentsController < ApplicationController
     @instrument = Instrument.find(params[:id])
 
     if @instrument.update(instrument_params)
-      redirect_to @instrument
+      redirect_to shop_index_path
     else
       render :edit
     end
@@ -49,13 +50,18 @@ class InstrumentsController < ApplicationController
   private
 
   def instrument_params
-    params.require(:instrument).permit(:name, :model, :brand, :price, :image, :photo)
+    params.require(:instrument).permit(:name, :model, :brand, :price, :image, :photo, :product_type_id)
   end
+
   def admin?
     if current_user.admin == true
       return true
     else 
       redirect_to root_path
     end
+  end
+
+  def set_product_type_options
+    @product_type_options = ProductType.all.pluck(:type_name, :id)
   end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,9 +1,10 @@
 class Instrument < ApplicationRecord
+  belongs_to :product_type
+
   validates :name, presence: true
   validates :model, presence: true
   validates :brand, presence: true
   validates :price, presence: true #, length: { minimum: 1 }
-  # validates :image, presence: true
 
   has_one_attached :photo
 end
