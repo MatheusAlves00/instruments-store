4c64fdcb090f5fedaf271fe7e7a3e42815de0855
matheusalves00 <m_alves11@yahoo.com.br>
2024-05-24 20:56:35 -0300

    Add relationship between models

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,5 +1,7 @@
 class Instrument < ApplicationRecord
   belongs_to :product_type
+  has_many :orderables
+  has_many :shopping_carts, through: :orderables
 
   validates :name, presence: true
   validates :model, presence: true
